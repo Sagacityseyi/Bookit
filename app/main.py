c7dd1ff8b8844b2d423f5ff24622286ab185b35f
Sagacityseyi <liganstickz@gmail.com>
2025-09-26 22:38:06 +0100

    ddd

--- app/main.py
+++ app/main.py
@@ -22,3 +22,7 @@ app.include_router(review_router)
 @app.get("/")
 def home():
     return {"message" : "Welcome home"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
