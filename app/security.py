c0cb31efdc624946322e51dbd5c1da0bc1965ce3
Sagacityseyi <liganstickz@gmail.com>
2025-09-26 23:03:56 +0100

    aa

--- app/security.py
+++ app/security.py
@@ -24,9 +24,13 @@ REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS", 7))
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    if len(password) > 72:
+        password = password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
+    if len(password) > 72:
+        password = password[:72]
     return pwd_context.hash(password)
 
 def create_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
