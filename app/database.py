e0f90239fdb34eff71fc0e048064623e6ea99e5a
Sagacityseyi <liganstickz@gmail.com>
2025-09-26 17:59:04 +0100

    ww

--- app/database.py
+++ app/database.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 load_dotenv()
-
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
